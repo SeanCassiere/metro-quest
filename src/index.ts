56fc0aa5cd8c9b05430779683c8263394fe26034
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2021-11-09 18:04:18 +0530

    feat(repo): added base footer and demo locations
    added the footer with the social icons, and rendering the locations using javascript

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,44 @@
 import { logger } from "./utils/logger";
 
+interface Location {
+  id: string;
+  name: String;
+}
+
+function populateLocationsData(): Location[] {
+  let locations: Location[] = [];
+  for (let i = 1; i < 10; i++) {
+    const location = {
+      id: `${i}`,
+      name: `Location ${i}`,
+    };
+    locations.push(location);
+  }
+  return locations;
+}
+
 export default function mainFunc() {
   logger("Application initialized");
-  const demoElem = document.getElementById("js-select");
 
-  if (demoElem) {
-    demoElem.innerHTML = "<b>Hello World from Typescript to Javascript conversion</b>";
+  const locationsRow = document.getElementById("demo-content-row");
+  if (locationsRow) {
+    const locations = populateLocationsData();
+    const locationsHtml = locations.map((location) => {
+      return `<div class="col-md-3">
+        <div class="card mb-4 shadow-sm">
+          <div class="card-body">
+            <p class="card-text">${location.name}</p>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>`;
+    });
+    locationsRow.innerHTML = locationsHtml.join("");
   }
 }
 
