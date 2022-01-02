53802adcf81aa4c6f8eaa399010e4b44d756ce50
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2022-01-02 18:57:34 +0530

    fix(user-service): comment out active methods in user-service test file
    comment out active methods in user-service test file
    
    BREAKING CHANGE: comment out active methods in the user-service test file

--- src/demo/userServiceTests.ts
+++ src/demo/userServiceTests.ts
@@ -1,22 +1,22 @@
 import UserService from "../services/UserService";
 
 export default async function userServiceTests() {
-  await UserService.registerNewUser({
-    firstName: "sean",
-    lastName: "cassiere",
-    email: "test@test.com",
-    password: "123",
-  });
+  // await UserService.registerNewUser({
+  //   firstName: "sean",
+  //   lastName: "cassiere",
+  //   email: "test@test.com",
+  //   password: "123",
+  // });
 
   const locallyStoredUsers = UserService.getAllUsersAsArray();
   if (locallyStoredUsers) {
     console.log("Locally stored users\n", locallyStoredUsers);
   }
 
-  UserService.loginUser({ email: "test@test.com", password: "123" });
+  // UserService.loginUser({ email: "test@test.com", password: "123" });
 
-  const loggedInUser = UserService.getLoggedInUser();
-  console.log("get logged in user", loggedInUser);
+  // const loggedInUser = UserService.getLoggedInUser();
+  // console.log("get logged in user", loggedInUser);
 
-  console.log("find user by id", UserService.getUserById(loggedInUser?.id ?? ""));
+  // console.log("find user by id", UserService.getUserById(loggedInUser?.id ?? ""));
 }
