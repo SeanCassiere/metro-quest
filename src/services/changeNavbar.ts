9a5e4f0fbaff813b0721fec42d4a4fed382edc13
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2022-01-05 10:06:07 +0530

    refactor(navbar): refactor dynamic navbar function
    re #23

--- src/services/changeNavbar.ts
+++ src/services/changeNavbar.ts
@@ -1,69 +1,19 @@
 import UserService, { User } from "./UserService.js";
 
 export function dynamicNavbar(user: User | null) {
-  const primaryItems = jQuery("#primary-nav-items");
+  const primaryItemsSelector = jQuery("#primary-nav-items");
   const logoutModal = jQuery("#logout-modal");
 
   if (logoutModal) {
     console.log("logoutModal", logoutModal);
-    if (user) {
-      logoutModal.html(`
-      <!-- Logout User Modal -->
-      <div
-        class="modal fade"
-        id="staticBackdrop"
-        data-bs-backdrop="static"
-        data-bs-keyboard="false"
-        tabindex="-1"
-        aria-labelledby="staticBackdropLabel"
-        aria-hidden="true"
-      >
-        <div class="modal-dialog modal-dialog-centered" style="z-index: 100;">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="staticBackdropLabel">Logout?</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-              <div class="modal-body">
-                <span>Are you sure you want to logout?</span>
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-metro-blue" data-bs-dismiss="modal">Dismiss</button>
-                <button type="button" class="btn btn-metro-orange text-white" data-bs-dismiss="modal" id="logout-click">Logout</button>
-              </div>
-          </div>
-        </div>
-      </div>
-      `);
-    } else {
-      logoutModal.html("");
-    }
+    logoutModal.html(stringLogoutModal(user));
   }
 
-  if (primaryItems) {
-    if (user) {
-      primaryItems.html(`
-
-        <li class="nav-item">
-          <a class="nav-link pe-2" aria-current="page" href="/account.html?tab=profile">Hi, ${user.firstName}</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#staticBackdrop" style="padding-left: 0.5rem;"><i class="fa fa-sign-out" aria-hidden="true"></i></a>
-        </li>
-        
-        `);
-    } else {
-      primaryItems.html(`
-      <li class="nav-item">
-        <a class="nav-link pe-2" aria-current="page" href="/login.html">Login</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/register.html">Sign Up</a>
-      </li>
-      `);
-    }
+  if (primaryItemsSelector) {
+    primaryItemsSelector.html(stringPrimaryNavItems(user));
   }
 
+  // event listener for the logout confirmation call
   const logoutListener = jQuery("#logout-click");
   if (logoutListener) {
     logoutListener.on("click", () => {
@@ -74,34 +24,65 @@ export function dynamicNavbar(user: User | null) {
   }
 }
 
-function javaScriptImplementation() {
-  // const primaryItems = document.getElementById("primary-nav-items");
-  // if (primaryItems) {
-  //   if (user) {
-  //     primaryItems.innerHTML = `
-  //     <li class="nav-item">
-  //       <a class="nav-link pe-2" aria-current="page" href="#">Account</a>
-  //     </li>
-  //     <li class="nav-item">
-  //       <a class="nav-link" href="#" id="logout-click">Log Out</a>
-  //     </li>
-  //     `;
-  //   } else {
-  //     primaryItems.innerHTML = `
-  //     <li class="nav-item">
-  //       <a class="nav-link pe-2" aria-current="page" href="#">Login</a>
-  //     </li>
-  //     <li class="nav-item">
-  //       <a class="nav-link" href="#">Sign Up</a>
-  //     </li>
-  //     `;
-  //   }
-  // }
-  // const logoutListener = document.getElementById("logout-click");
-  // if (logoutListener) {
-  //   logoutListener.addEventListener("click", () => {
-  //     UserService.logoutUser();
-  //     dynamicNavbar(UserService.getLoggedInUser());
-  //   });
-  // }
+function stringPrimaryNavItems(user: User | null) {
+  if (user) {
+    return `
+
+    <li class="nav-item">
+      <a class="nav-link pe-2" aria-current="page" href="/account.html?tab=profile">Hi, ${user.firstName}</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#staticBackdrop" style="padding-left: 0.5rem;"><i class="fa fa-sign-out" aria-hidden="true"></i></a>
+    </li>
+
+    `;
+  }
+
+  return `
+  
+  <li class="nav-item">
+    <a class="nav-link pe-2" aria-current="page" href="/login.html">Login</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="/register.html">Sign Up</a>
+  </li>
+
+  `;
+}
+
+function stringLogoutModal(user: User | null) {
+  if (user) {
+    return `
+
+    <!-- Logout User Modal -->
+    <div
+      class="modal fade"
+      id="staticBackdrop"
+      data-bs-backdrop="static"
+      data-bs-keyboard="false"
+      tabindex="-1"
+      aria-labelledby="staticBackdropLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-dialog-centered" style="z-index: 100;">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="staticBackdropLabel">Logout?</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+            <div class="modal-body">
+              <span>Are you sure you want to logout?</span>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-metro-blue" data-bs-dismiss="modal">Dismiss</button>
+              <button type="button" class="btn btn-metro-orange text-white" data-bs-dismiss="modal" id="logout-click">Logout</button>
+            </div>
+        </div>
+      </div>
+    </div>
+
+    `;
+  }
+
+  return "";
 }
