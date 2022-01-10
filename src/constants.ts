20ba844b282731f0f4feec9c67869aa14190ad5c
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2022-01-10 21:47:31 +0530

    fix(user-service): not loading environment file
    not loading the environment fetching file
    
    re #39

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-import { getServerUrls } from "./utils/environment";
+import { getServerUrls } from "./utils/environment.js";
 
 export const APP_PREFIX = "MQ";
 export const UUID_URI_OLD = "https://www.uuidtools.com/api/generate/v4";
